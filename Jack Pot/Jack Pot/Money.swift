44344635e919063c6db0fda6091a005ecbbde87b
Keron Williams <keronwilliams9@gmail.com>
2015-10-13 21:33:09 -0400

    Initial Jackpot assignment 01- first steps

---
+++ Jack Pot/Jack Pot/Money.swift
@@ -0,0 +1,15 @@
+//
+//  Money.swift
+//  Jack Pot
+//
+//  Created by Keron Williams on 10/13/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import Foundation
+
+
+class Money
+{
+
+}
