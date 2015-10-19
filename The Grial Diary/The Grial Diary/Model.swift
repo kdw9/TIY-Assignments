a9a1a32d02cfb18f93e2f457ff136c85fd82fc98
Keron Williams <keronwilliams9@gmail.com>
2015-10-19 14:45:53 -0400

    initial entry for the Grail Diary HomeWork

---
+++ The Grial Diary/The Grial Diary/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  The Grial Diary
+//
+//  Created by Keron Williams on 10/19/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import Foundation
+
+
+class Model
+
+{
+
+}
---
+++ The Grial Diary/The Grial DiaryTests/The_Grial_DiaryTests.swift
@@ -0,0 +1,36 @@
+//
+//  The_Grial_DiaryTests.swift
+//  The Grial DiaryTests
+//
+//  Created by Keron Williams on 10/19/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import XCTest
+@testable import The_Grial_Diary
+
+class The_Grial_DiaryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
